b22046b0d136202bdc53e77d8612f6bb2de75214
Britnie14 <niebrit14@gmail.com>
2024-10-03 16:17:23 +0800

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,7 +5,6 @@ const Home: React.FC = () => {
     <div className="flex-1 p-6 bg-gray-100 min-h-screen mt-16">
       <h1 className="text-3xl font-bold mb-4">Home Dashboard</h1>
       <p className="text-lg mb-6">
-        James F. Galos
       </p>
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mb-6">
